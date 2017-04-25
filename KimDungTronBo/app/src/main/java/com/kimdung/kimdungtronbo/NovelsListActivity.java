9dc77aa387b4cac85d0e4948d510267d58e09a62
giapminhcuong <cuonggm48@gmail.com>
2017-04-25 11:00:20 +0700

    init_project

---
+++ KimDungTronBo/app/src/main/java/com/kimdung/kimdungtronbo/NovelsListActivity.java
@@ -0,0 +1,13 @@
+package com.kimdung.kimdungtronbo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NovelsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novels_list);
+    }
+}
